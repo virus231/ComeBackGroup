d61bf012b7f1465121bf1e5d1819278fa423a246
Vlad <vladprodan6@gmail.com>
2021-02-26 02:59:41 +0200

    add fonts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 
-import './fonts/libre-baskerville-v5-latin-regular.woff';
-import './fonts/libre-baskerville-v5-latin-regular.woff2';
+import './fonts/Gilroy-Regular.woff';
+import './fonts/Gilroy-Regular.woff2';
 
 import './index.html';
 import './index.scss';
